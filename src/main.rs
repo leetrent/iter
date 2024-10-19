de3f081e8fe0611652179119f6d9928931d2b27e
Lee Trent <lee.trent.1@gmail.com>
2024-10-19 10:36:35 -0500

    Using both mutable Vector and string slice references

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
-fn shorten_strings(elements: &mut Vec<String>) {
+fn shorten_strings_using_vec(elements: &mut Vec<String>) {
     elements.iter_mut().for_each(|el| el.truncate(1));
 }
 
+fn shorten_strings_using_slice(elements: &mut [String]) {
+    elements.iter_mut().for_each(|el| el.truncate(1));
+}
 fn main() {
+    use_vec();
+    use_slice();
+}
+
+fn use_vec() {
     let mut colors = vec! [
         String::from("red"),
         String::from("green"),
@@ -11,7 +19,22 @@ fn main() {
 
     // BEFORE
     println!("{:#?}", colors);
-    shorten_strings(&mut colors);
+    shorten_strings_using_vec(&mut colors);
+
+    // AFTER
+    println!("{:#?}", colors)
+}
+
+fn use_slice() {
+    let mut colors = vec! [
+        String::from("red"),
+        String::from("green"),
+        String::from("blue")
+    ];
+
+    // BEFORE
+    println!("{:#?}", colors);
+    shorten_strings_using_slice(&mut colors[1..3]);
 
     // AFTER
     println!("{:#?}", colors)

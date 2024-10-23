7941e966265563b533ef9f9f216fa78b617eb3ba
Lee Trent <lee.trent.1@gmail.com>
2024-10-22 20:35:04 -0500

    Completed project

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,26 @@
-fn shorten_strings_using_vec(elements: &mut Vec<String>) {
-    elements.iter_mut().for_each(|el| el.truncate(1));
+// fn to_uppercase(elements: &[String]) -> Vec<String> {
+//     elements
+//     .iter()
+//     .map(|el| el.to_uppercase())
+//     .collect()
+// }
+
+fn to_uppercase(elements: &[String]) -> Vec<String> {
+    elements
+    .iter()
+    .map(|el| el.to_uppercase())
+    .collect::<Vec<String>>()
 }
 
-fn shorten_strings_using_slice(elements: &mut [String]) {
-    elements.iter_mut().for_each(|el| el.truncate(1));
-}
 fn main() {
-    use_vec();
-    use_slice();
-}
-
-fn use_vec() {
-    let mut colors = vec! [
+    let colors = vec! [
         String::from("red"),
         String::from("green"),
         String::from("blue")
     ];
 
-    // BEFORE
     println!("{:#?}", colors);
-    shorten_strings_using_vec(&mut colors);
-
-    // AFTER
-    println!("{:#?}", colors)
+    let uppercased = to_uppercase(&colors);
+    println!("{:#?}", uppercased)
 }
 
-fn use_slice() {
-    let mut colors = vec! [
-        String::from("red"),
-        String::from("green"),
-        String::from("blue")
-    ];
-
-    // BEFORE
-    println!("{:#?}", colors);
-    shorten_strings_using_slice(&mut colors[1..3]);
-
-    // AFTER
-    println!("{:#?}", colors)
-}
